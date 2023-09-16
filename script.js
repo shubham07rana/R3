832f8a0fb3b325f8e7aa90f38a0b0a7dd0de648b
shubham07rana <shubahmkumarrana17036@gmail.com>
2023-09-16 20:50:37 +0530

    F2REP03

---
+++ script.js
@@ -0,0 +1,84 @@
+let arr = [
+    { id: 1, name: 'john', age: 18, profession: 'developer', salary: 1000 },
+    { id: 2, name: 'jack', age: 20, profession: 'developer', salary: 1100 },
+    { id: 3, name: 'karen', age: 19, profession: 'admin', salary: 900 },
+  ];
+  
+  // 1. Print all developers from the array whose salary is above the average salary of the array.
+  function PrintHighEarningDevelopers() {
+    const totalSalary = developers.reduce((sum, developer) => sum + developer.salary, 0);
+    const averageSalary = totalSalary / developers.length;
+    return developers.filter((developer) => developer.salary > averageSalary);
+}
+    //Write your code here , just console.log
+  
+  
+  // 2. Using a recursive function, add a new data object to the array until the user says 'no' via a prompt.
+  function recursiveAddData() {
+    let addnew = { id: 4, name: "Shubham", age: "22", profession: "software Engineer" };
+    arr.push(addnew);
+       for(let i=0;i<arr.length;i++){
+          console.log(arr[i]);
+       }
+    //Write your code here, just console.log
+  }
+  
+  // 3. Remove the youngest and oldest person from the array.
+  function removeYoungestAndOldest() {
+    //Write your code here, just console.log
+  }
+  
+  // 4. Merge two arrays (use a dummy array for demonstration) such that the resulting array has no duplicate professions.
+  function mergeAndFilterProfessions() {
+    //Write your code here, just console.log
+  }
+  
+  // 5. Find the person with the highest salary and reduce their salary by 10% as tax.
+  function applyTax() {
+    //Write your code here, just console.log
+  }
+  
+  // 6. Check if every person in the array is a developer.
+  function isEveryoneDeveloper() {
+    //Write your code here, just console.log
+  }
+  
+  // 7. Swap the professions of the first and last person in the array.
+  function swapProfessions() {
+    //Write your code here, just console.log
+  }
+  
+  // 8. Using the `map` method, add a new property to each person called 'experience' with a default value of 1 year.
+  function addExperienceProperty() {
+    //Write your code here, just console.log
+  }
+  
+  // 9. Group persons by profession and count them. E.g., {developer: 2, admin: 1}.
+  function groupByProfession() {
+    //Write your code here, just console.log
+  }
+  
+  // 10. Find the person who has a name with the longest length and capitalize their profession.
+  function capitalizeLongestNamedPersonProfession() {
+    //Write your code here, just console.log
+  }
+  
+  // 11. Create a function that shuffles the array in a random order.
+  function shuffleArray() {
+    //Write your code here, just console.log
+  }
+  
+  // 12. Create a function that rotates the array to the right by n steps, where n is provided as an argument.
+  function rotateArray(n) {
+    //Write your code here, just console.log
+  }
+  
+  // 13. Create a function that finds the person with the closest age to a given number.
+  function findClosestAge(age) {
+    //Write your code here, just console.log
+  }
+  
+  // 14. Create a function that checks if there's any repeated name in the array.
+  function hasRepeatedNames() {
+    //Write your code here, just console.log
+  }
